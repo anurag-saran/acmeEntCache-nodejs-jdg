f9e19c9a222929b315050b6469e44786f14257cb
gptestudent <gptestudent@gptestudent.com>
2017-04-21 10:23:35 -0400

    Moved connection inside

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/', function(req, res) {
 });
 
 app.listen(PORT, function() {
-		console.log('Express listening on port ' + PORT + '!');
+		console.log('Express listening on port : ' + PORT + '!');
 	});
 
 
