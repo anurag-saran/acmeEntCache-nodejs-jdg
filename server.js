3d806d829ffe5009368a0651b7f13e759c04314c
gptestudent <gptestudent@gptestudent.com>
2017-04-21 10:44:09 -0400

    comented code

--- server.js
+++ server.js
@@ -1,40 +1,51 @@
-'use strict';
-var infinispan = require('infinispan');
-var util = require('util');
-//var app = require('express')();
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
 
+var infinispan = require('infinispan');
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+var util = require('util');
+var bodyParser = require('body-parser');
 var jdgHost = process.env.DATAGRID_HOTROD_SERVICE_HOST || "127.0.0.1";
 var jdgPort = process.env.DATAGRID_HOTROD_SERVICE_PORT || 11222;
-var PORT = process.env.PORT || 8080;
-app.use(bodyParser.json);
 
+//app.use(bodyParser.json);
 
+app.get('/', function(req, res) {
+    console.log("****/:");
+	res.send('Webpage API Root');
+});
+
+app.get('/test', function(req, res) {
+    console.log("****/test:");
+	res.send('Webpage API Root');
+});
 
 //// GET /customer/:id
-//app.get('/customer/:id', function(req, res) {
-//    console.log("*****// GET /customer/:id.")
-//    var custID = req.params.id;
-//    var connected = infinispan.client({port: jdgPort, host: jdgHost}, {version: '2.2'});
-//	connected.then(function (client) {
-//        client.get(custID).then(
-//            function(value) {
-//                if(value == undefined)  {
-//                    console.log("*****Record Not Found.")
-//                    //res.json(util.format('Customer Not Found %s!', custID));
-//                    res.status(404).send();
-//                } else {
-//                    console.log("*****Record Found.")
-//                    res.json(value);
-//                    
-//                }
-//            })
-//        })
-//})
-//
-//// POST /customer
+app.get('/customer/:id', function(req, res) {
+    console.log("*****// GET /customer/:id.");
+    var custID = req.params.id;
+    var connected = infinispan.client({port: jdgPort, host: jdgHost}, {version: '2.2'});
+    console.log("#### connected"+JSON.stringify(connected))
+    
+	connected.then(function (client) {
+        client.get(custID).then(
+            function(value) {
+                if(value == undefined)  {
+                    console.log("*****Record Not Found.");
+                    client.put(custID, "abc");
+                    //res.json(util.format('Customer Not Found %s!', custID));
+                    res.status(404).send();
+                } else {
+                    console.log("*****Record Found.");
+                    res.json(value);
+                    
+                }
+            });
+        });
+});
+
+// POST /customer
 //app.post('/customer', function(req, res) {
 //    var body = reg.body;
 //    var custID = body.custID;
@@ -51,16 +62,8 @@ app.use(bodyParser.json);
 //        })
 //})
 
-
-
-
-app.get('/', function(req, res) {
-    console.log("****/:");
-	res.send('Webpage API Root');
-});
-
 app.listen(PORT, function() {
-		console.log('Express listening on port : ' + PORT + '!');
+		console.log('Express listening on port :' + PORT + '!');
 	});
 
 
