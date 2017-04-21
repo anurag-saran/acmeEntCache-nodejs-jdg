b4202a3a44c34461541d312c93864f918f9ed6ea
gptestudent <gptestudent@gptestudent.com>
2017-04-21 10:20:18 -0400

    Moved connection inside

--- server.js
+++ server.js
@@ -13,48 +13,49 @@ app.use(bodyParser.json);
 
 
 
-// GET /customer/:id
-app.get('/customer/:id', function(req, res) {
-    console.log("*****// GET /customer/:id.")
-    var custID = req.params.id;
-    var connected = infinispan.client({port: jdgPort, host: jdgHost}, {version: '2.2'});
-	connected.then(function (client) {
-        client.get(custID).then(
-            function(value) {
-                if(value == undefined)  {
-                    console.log("*****Record Not Found.")
-                    //res.json(util.format('Customer Not Found %s!', custID));
-                    res.status(404).send();
-                } else {
-                    console.log("*****Record Found.")
-                    res.json(value);
-                    
-                }
-            })
-        })
-})
-
-// POST /customer
-app.post('/customer', function(req, res) {
-    var body = reg.body;
-    var custID = body.custID;
-    console.log("****body:"+body);
-    var connected = infinispan.client({port: jdgPort, host: jdgHost}, {version: '2.2'});
-	connected.then(function (client) {
-        client.get(custID).then(
-            function(value) {
-                if(value == undefined)  {
-                    client.put(custID, body)
-                    res.json(util.format('Customer Not Found %s! but inserted into cache now', custID));
-                }
-            })
-        })
-})
+//// GET /customer/:id
+//app.get('/customer/:id', function(req, res) {
+//    console.log("*****// GET /customer/:id.")
+//    var custID = req.params.id;
+//    var connected = infinispan.client({port: jdgPort, host: jdgHost}, {version: '2.2'});
+//	connected.then(function (client) {
+//        client.get(custID).then(
+//            function(value) {
+//                if(value == undefined)  {
+//                    console.log("*****Record Not Found.")
+//                    //res.json(util.format('Customer Not Found %s!', custID));
+//                    res.status(404).send();
+//                } else {
+//                    console.log("*****Record Found.")
+//                    res.json(value);
+//                    
+//                }
+//            })
+//        })
+//})
+//
+//// POST /customer
+//app.post('/customer', function(req, res) {
+//    var body = reg.body;
+//    var custID = body.custID;
+//    console.log("****body:"+body);
+//    var connected = infinispan.client({port: jdgPort, host: jdgHost}, {version: '2.2'});
+//	connected.then(function (client) {
+//        client.get(custID).then(
+//            function(value) {
+//                if(value == undefined)  {
+//                    client.put(custID, body)
+//                    res.json(util.format('Customer Not Found %s! but inserted into cache now', custID));
+//                }
+//            })
+//        })
+//})
 
 
 
 
 app.get('/', function(req, res) {
+    console.log("****/:");
 	res.send('Webpage API Root');
 });
 
