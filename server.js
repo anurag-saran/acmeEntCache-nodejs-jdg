6d171ff391f2d00c200129e9a38419cad0a62075
gptestudent <gptestudent@gptestudent.com>
2017-04-23 15:25:27 -0400

    commented code

--- server.js
+++ server.js
@@ -9,41 +9,41 @@ var bodyParser = require('body-parser');
 var jdgHost = process.env.DATAGRID_HOTROD_SERVICE_HOST || "127.0.0.1";
 var jdgPort = process.env.DATAGRID_HOTROD_SERVICE_PORT || 11222;
 
-//app.use(bodyParser.json);
+app.use(bodyParser.json);
 
 app.get('/', function(req, res) {
     console.log("****/:");
-	res.send('Webpage API Root');
+	res.send('Webpage API Root /');
 });
 
 app.get('/test', function(req, res) {
     console.log("****/test:");
-	res.send('Webpage API Root');
+	res.send('Webpage API Root /test');
 });
 
 //// GET /customer/:id
-app.get('/customer/:id', function(req, res) {
-    console.log("*****// GET /customer/:id.");
-    var custID = req.params.id;
-    var connected = infinispan.client({port: jdgPort, host: jdgHost}, {version: '2.2'});
-    console.log("#### connected"+JSON.stringify(connected))
-    
-	connected.then(function (client) {
-        client.get(custID).then(
-            function(value) {
-                if(value == undefined)  {
-                    console.log("*****Record Not Found.");
-                    client.put(custID, "abc");
-                    //res.json(util.format('Customer Not Found %s!', custID));
-                    res.status(404).send();
-                } else {
-                    console.log("*****Record Found.");
-                    res.json(value);
-                    
-                }
-            });
-        });
-});
+//app.get('/customer/:id', function(req, res) {
+//    console.log("*****// GET /customer/:id.");
+//    var custID = req.params.id;
+//    var connected = infinispan.client({port: jdgPort, host: jdgHost}, {version: '2.2'});
+//    console.log("#### connected"+JSON.stringify(connected))
+//    
+//	connected.then(function (client) {
+//        client.get(custID).then(
+//            function(value) {
+//                if(value == undefined)  {
+//                    console.log("*****Record Not Found.");
+//                    client.put(custID, "abc");
+//                    //res.json(util.format('Customer Not Found %s!', custID));
+//                    res.status(404).send();
+//                } else {
+//                    console.log("*****Record Found.");
+//                    res.json(value);
+//                    
+//                }
+//            });
+//        });
+//});
 
 // POST /customer
 //app.post('/customer', function(req, res) {
